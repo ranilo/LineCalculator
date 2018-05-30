56739addc9ea2a1a468974b363be3f86d5025d9a
ranilo <ranilo@users.noreply.github.com>
2018-05-31 00:14:53 +0300

    client end done

--- public/app.js
+++ public/app.js
@@ -14,6 +14,84 @@ document.addEventListener("DOMContentLoaded", event=>
 
     const lineTypes = document.querySelector('#lineType');
     const sailingStyle = document.querySelector('#sailingStyle');
+    const ftRecommendationString = document.querySelector('#ftrecommendationString');
+    const mmRecommendationString = document.querySelector('#mmrecommendationString');
+
+
+    const meterSelector = document.querySelector('#meter');
+    const feetSelector = document.querySelector('#ft');
+    const boatLengthNumberSelector = document.querySelector('.boatLengthNumber');
+    
+    const lineTypeListSelector = document.querySelector('.lineTypeList');
+    const sailingStyleListSelectpr = document.querySelector('.sailingStyleList');
+
+    const sailingStyleDiv = document.querySelector('.sailingStyle');
+    const lineTypeDiv = document.querySelector('.lineType');
+    const recomndationDiv = document.querySelector('.recomndation');
+
+    var boatLengtft;
+    var boatLengtmm;
+    
+    
+    var hideAll = () =>{
+    document.querySelector('.boatLengthNumberError').style.visibility = "hidden";
+    sailingStyleDiv.style.visibility = "hidden";
+    lineTypeDiv.style.visibility = "hidden";
+    recomndationDiv.style.visibility = "hidden";
+    };
+
+    var showAll = () =>{
+        document.querySelector('.boatLengthNumberError').style.visibility = "visible";
+        sailingStyleDiv.style.visibility = "visible";
+        lineTypeDiv.style.visibility = "visible";
+        recomndationDiv.style.visibility = "visible";
+    };
+
+    hideAll();
+
+    var check = ()=>{
+        recomndationDiv.style.visibility = "hidden";
+        if(boatClassValue<=0) {hideAll(); return;}
+        if(sailingStyleListSelectpr.value.length <=0) {hideAll(); sailingStyleDiv.style.visibility = "visible"; return;}
+        if(lineTypeListSelector.value.length <=0) { lineTypeDiv.style.visibility = "visible"; return;}
+
+        showAll();
+        getRopeByType(lineTypeListSelector.value, sailingStyleListSelectpr.value, boatClassValue);
+    };
+
+    lineTypeListSelector.addEventListener('change', event => check());
+    sailingStyleListSelectpr.addEventListener('change', event => check());
+
+
+    var updateBoatLength = ()=>
+    {
+        document.querySelector('.boatLengthNumberError').style.visibility = "hidden";
+        sailingStyleDiv.style.visibility = "hidden";
+        var length = boatLengthNumberSelector.value;
+        length = (meterSelector.checked)?length/0.3048:length; 
+        boatLengtft = length;
+        boatLengtmm = boatLengtft*0.3048;
+        boatClassValue = boatLengtft<=15? 3 : Math.ceil((boatLengtft-15)/5)+3;
+        if(boatClassValue > 10) 
+        {
+            document.querySelector('.boatLengthNumberError').style.visibility = "visible"; 
+            boatClassValue = 0;
+            hideAll();
+        }
+        console.log("boat class "+boatClassValue);  
+        sailingStyleDiv.style.visibility = "visible";
+        check();
+    };
+    boatLengthNumberSelector.addEventListener('change',event=>updateBoatLength());
+    
+
+    var boatLengthTypeChanged = (event) =>{
+        feetSelector.checked  = (event.currentTarget == feetSelector);
+        meterSelector.checked  = (event.currentTarget == meterSelector);
+        updateBoatLength();
+    };
+    meterSelector.addEventListener('change',event =>boatLengthTypeChanged(event));
+    feetSelector.addEventListener('change',event =>boatLengthTypeChanged(event));
 
     var boatClassValue;
 
@@ -28,17 +106,26 @@ document.addEventListener("DOMContentLoaded", event=>
         })
     });
 
-    var getRopeByType = type => {
-        var query = ropes.where('usageId', '==', type);
+
+    
+    var getRopeByType = (type, sailingTypeName, classId) => {
+        var query = ropes
+            .where('usage', '==', type)
+            .where('sailingType', '==', sailingTypeName)
+            .where('class', '==', classId);
 
         query.get()
         .then(rope =>{
             rope.forEach(doc=>{
                 data = doc.data()
-                console.log([data, data.usage]);
+                console.log(data);
+                ftRecommendationString.innerHTML = `${data.usage} ${Math.round(boatLengtft*data.lengthFactor)} feet Double braided ${data.typeOfRope} rope diameter ${data.inch} inch`;
+                mmRecommendationString.innerHTML = `${data.usage} ${Math.round(boatLengtmm*data.lengthFactor)} meter Double braided ${data.typeOfRope} rope diameter ${data.mm} mm`;
+                updateRecomendtionView();
             })
         })
     };
+   
 
     var addToDataList = (data,list) =>{
         var option = document.createElement("OPTION");
@@ -47,31 +134,30 @@ document.addEventListener("DOMContentLoaded", event=>
         list.appendChild(option);
     }
 
-    var calulateClass = (lnegth, inFeet) =>{
-        length = inFeet?length:mft.get(length);        
-        console.log(length);
+    var ftRec = document.querySelector('#ftRecomenation');
+    var mmRec = document.querySelector('#meterRecomendation');
+    var recomendationChange = (event) =>{
+        ftRec.checked  = (event.currentTarget == ftRec);
+        mmRec.checked  = (event.currentTarget == mmRec);
+        updateRecomendtionView();
     };
+
+    var updateRecomendtionView = () => {
+        mmRecommendationString.style.visibility = mmRec.checked?"visible":"hidden";
+        ftRecommendationString.style.visibility = ftRec.checked?"visible":"hidden";
+    };
+
+    ftRec.addEventListener('change', event => recomendationChange(event));
+    mmRec.addEventListener('change', event => recomendationChange(event));
+
+
+
+
     
 
 });
 
-var meterSelector = document.querySelector('#meter');
-var feetSelector = document.querySelector('#ft');
-     
-function updateBoatLength(event){
 
-    var length = document.querySelector('.boatLengthNumber').value;
-    length = (meterSelector.checked)?length/0.3048:length; 
-    boatClassValue = length<=15? 3 : Math.ceil((length-15)/5)+3;
-    console.log("boat class "+boatClassValue);  
-  }
-
-function boatLengthTypeChanged(event){
-    feetSelector.checked  = (event.currentTarget == feetSelector);
-    meterSelector.checked  = (event.currentTarget == meterSelector);
-    updateBoatLength();
-
-}
 
 
 
